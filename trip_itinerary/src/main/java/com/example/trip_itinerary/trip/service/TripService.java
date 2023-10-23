99aad1276b8f16f6395f58fd42f06b7d95321163
kwonMInwoo <qwerty029369@naver.com>
2023-10-23 15:51:01 +0900

    Feat: Trip layer 생성
    여행 도메인의 Controller, Service, Repository를 생성

---
+++ trip_itinerary/src/main/java/com/example/trip_itinerary/trip/service/TripService.java
@@ -0,0 +1,11 @@
+package com.example.trip_itinerary.trip.service;
+
+import com.example.trip_itinerary.trip.domain.Trip;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface TripService {
+    public Trip saveTrip();
+
+
+}
