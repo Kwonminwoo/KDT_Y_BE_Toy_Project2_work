69c21bcb22023841b0435c1ff7c84f9ecabc5abc
KwonMinwoo <qwerty029369@naver.com>
2023-10-27 17:58:29 +0900

    Feat: 스웨거 적용 테스트
    스웨거 의존성 추가 및  테스트로 작성

--- trip_itinerary/src/main/java/com/example/trip_itinerary/trip/controller/TripController.java
+++ trip_itinerary/src/main/java/com/example/trip_itinerary/trip/controller/TripController.java
@@ -7,6 +7,10 @@ import com.example.trip_itinerary.trip.dto.response.TripFindResponse;
 import com.example.trip_itinerary.trip.dto.response.TripListFindResponse;
 import com.example.trip_itinerary.trip.service.TripService;
 import com.example.trip_itinerary.util.DateUtil;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,6 +26,14 @@ public class TripController {
         this.tripService = tripService;
     }
 
+    @ApiOperation(value = "여행 저장", notes = "여행 정보를 입력받아 저장한다")
+    @ApiResponses(
+            @ApiResponse(code = 200, message = "저장 성공")
+    )
+    @ApiImplicitParam(
+            name = "여행 정보",
+            value = "이름, 시작 날짜, 종료 날짜, 국내외 여부"
+    )
     @PostMapping
     public Long saveTrip(@RequestBody @Validated TripSaveRequest tripSaveRequest) {
         return tripService.saveTrip(tripSaveRequest);
